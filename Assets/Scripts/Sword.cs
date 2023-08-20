3dff81060fbf39cd6769ba0f78f52fe43fa7216a
keegenwall-is <keegen.wallis@gmail.com>
2023-08-21 09:52:13 +1000

    Collider Moves
    with player attack direction

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -18,6 +18,14 @@ public class Sword : MonoBehaviour
 
     int attackCount = 1;
 
+    Vector2 upColliderOffset = new Vector2(0, 2);
+    Vector2 leftColliderOffset = new Vector2(-2, 0);
+    Vector2 downColliderOffset = new Vector2(0, -2);
+    Vector2 rightColliderOffset = new Vector2(2, 0);
+
+    Vector2 verticalColliderSize = new Vector2(6, 3);
+    Vector2 horizontalColliderSize = new Vector2(3, 6);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,15 +52,27 @@ public class Sword : MonoBehaviour
                     {
                         case 1:
                             anim.Play("SwordAttack1UpAnim");
+                            cc.offset = upColliderOffset;
+                            cc.size = verticalColliderSize;
+                            cc.direction = CapsuleDirection2D.Horizontal;
                             break;
                         case 2:
                             anim.Play("SwordAttack1LeftAnim");
+                            cc.offset = leftColliderOffset;
+                            cc.size = horizontalColliderSize;
+                            cc.direction = CapsuleDirection2D.Vertical;
                             break;
                         case 3:
                             anim.Play("SwordAttack1Anim");
+                            cc.offset = downColliderOffset;
+                            cc.size = verticalColliderSize;
+                            cc.direction = CapsuleDirection2D.Horizontal;
                             break;
                         case 4:
                             anim.Play("SwordAttack1RightAnim");
+                            cc.offset = rightColliderOffset;
+                            cc.size = horizontalColliderSize;
+                            cc.direction = CapsuleDirection2D.Vertical;
                             break;
                     }
                     attackCount++;
@@ -65,15 +85,27 @@ public class Sword : MonoBehaviour
                     {
                         case 1:
                             anim.Play("SwordAttack2UpAnim");
+                            cc.offset = upColliderOffset;
+                            cc.size = verticalColliderSize;
+                            cc.direction = CapsuleDirection2D.Horizontal;
                             break;
                         case 2:
                             anim.Play("SwordAttack2LeftAnim");
+                            cc.offset = leftColliderOffset;
+                            cc.size = horizontalColliderSize;
+                            cc.direction = CapsuleDirection2D.Vertical;
                             break;
                         case 3:
                             anim.Play("SwordAttack2DownAnim");
+                            cc.offset = downColliderOffset;
+                            cc.size = verticalColliderSize;
+                            cc.direction = CapsuleDirection2D.Horizontal;
                             break;
                         case 4:
                             anim.Play("SwordAttack2RightAnim");
+                            cc.offset = rightColliderOffset;
+                            cc.size = horizontalColliderSize;
+                            cc.direction = CapsuleDirection2D.Vertical;
                             break;
                     }
                     attackCount--;
